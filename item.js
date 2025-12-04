e9d9516b03b55d3c5e8b64ce72bb67f24a755b83
Shopp-Wholesale <aqc.cloudproject@gmail.com>
2025-12-04 18:42:30 +0530

    Create item.js

---
+++ item.js
@@ -0,0 +1,30 @@
+[
+  {
+    "id": 1,
+    "name": "Sugar 1kg",
+    "mrp": 50,
+    "salePrice": 42,
+    "image": "images/sugar1kg.png"
+  },
+  {
+    "id": 2,
+    "name": "Sunflower Oil 1L",
+    "mrp": 140,
+    "salePrice": 118,
+    "image": "images/oil1l.png"
+  },
+  {
+    "id": 3,
+    "name": "Atta 5kg",
+    "mrp": 420,
+    "salePrice": 399,
+    "image": "images/atta5kg.png"
+  },
+  {
+    "id": 4,
+    "name": "Bread (400g)",
+    "mrp": 40,
+    "salePrice": 36,
+    "image": "images/bread400g.png"
+  }
+]
